a5356c83adcdfd444c10d036b8d73aa7050fcb6b
陈晓龙 <909372455@qq.com>
2021-05-10 23:40:53 +0800

    避免冲突

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
+        System.out.println("a");
     }
 }
