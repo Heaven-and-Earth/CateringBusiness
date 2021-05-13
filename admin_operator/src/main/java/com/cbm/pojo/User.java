c80827d7ec4a8ae68fd46bf55b5da8f68302bc5e
陈晓龙 <909372455@qq.com>
2021-05-13 10:52:28 +0800

    管理员登录模块

---
+++ admin_operator/src/main/java/com/cbm/pojo/User.java
@@ -0,0 +1,20 @@
+package com.cbm.pojo;
+
+import lombok.Data;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @date 2021/5/13 10:33
+ * @Version 1.0
+ */
+@Data
+public class User {
+
+    private String id;
+
+    private String username;
+
+    private String password;
+
+}
